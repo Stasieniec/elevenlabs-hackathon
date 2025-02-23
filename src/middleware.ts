6727839ee8681b49b4d817c9c9d89d52ba3009df
Stasieniec <wasilewski.sf@gmail.com>
2025-02-23 03:45:43 +0100

    maybe

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,8 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
+// Disable debug mode to avoid header encoding issues
 export default clerkMiddleware();
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!_next/static|_next/image|_vercel|favicon.ico).*)'],
 }; 
