38860401bfa7f1e384dfb43459e29125451b92d0
Stasieniec <wasilewski.sf@gmail.com>
2025-02-23 03:45:56 +0100

    ok i guess next js will not tolerate polish names

--- src/app/HomeContent.tsx
+++ src/app/HomeContent.tsx
@@ -497,7 +497,7 @@ export default function HomeContent() {
                           <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
                         </svg>
                         <a href="https://www.linkedin.com/in/stanislaw-wasilewski/" target="_blank" className="hover:text-[#27AE60] transition-colors">
-                          Stanisław Wasilewski
+                          Stanislaw Wasilewski
                         </a>
                       </div>
                     </div>
@@ -508,7 +508,7 @@ export default function HomeContent() {
                 </div>
 
                 <div className="border-t border-gray-100 mt-12 pt-8 text-center text-[#34495E] text-sm">
-                  <p>Created by Stanisław Wasilewski for ElevenLabs x a16z AI Agents Hackathon 2025</p>
+                  <p>Created by Stanislaw Wasilewski for ElevenLabs x a16z AI Agents Hackathon 2025</p>
                 </div>
               </div>
             </footer>
